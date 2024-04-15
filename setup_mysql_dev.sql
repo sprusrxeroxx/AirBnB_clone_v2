314a2f77ecfa1319646d8e31d64318659b42681d
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-16 00:04:28 +0200

    task tweak

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -10,6 +10,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
 
 GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost'
 
 
 
