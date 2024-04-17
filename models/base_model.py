2d754e8ccc96598a6770af649f024b617df81430
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-18 00:55:09 +0200

    time instance error fix

--- models/base_model.py
+++ models/base_model.py
@@ -27,15 +27,19 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
-        for key, val in kwargs.items():
-            if "__class__" not in key:
-                setattr(self, key, val)
+            if kwargs.get("created_at"):
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.created_at = datetime.now()
+            if kwargs.get("updated_at"):
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.updated_at = datetime.now()
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
         """Returns a string representation of the instance"""
