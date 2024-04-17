ed2602b01f87ebf7085b111a30c52a8f562d9d6c
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-17 02:09:35 +0200

    Fixed mysql db import

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ from models.engine.file_storage import FileStorage
 import os
 
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    from models import DBStorage
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
 else:
