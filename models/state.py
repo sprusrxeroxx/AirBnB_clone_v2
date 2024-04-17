6c5e5a78f2b04ff6bf9b8d0e4feab2bddf331253
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-17 04:10:54 +0200

    Db ERROR

--- models/state.py
+++ models/state.py
@@ -2,11 +2,25 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from models.base_model import Base
-from sqlalchemy import ForeignKey, String, Column
+from sqlalchemy import String, Column, ForeignKey, relationship
+from models.engine.file_storage import FileStorage
+import os
 
 
 class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column("name", String(128), nullable=False)
-    """ State class """
-    name = ""
+
+    if 'DBStorage' in os.getenv("HBNB_TYPE_STORAGE"):
+        cities = relationship("City", backref="state", cascade="all, delete-orphan", single_use=True)
+
+    # Getter for FileStorage
+    else:
+        @property
+        def cities(self):
+            """
+            Returns a list of City instances linked to the current State.
+            """
+            from models.city import City  # Import City to avoid circular imports
+            all_cities = self@FileStorage.all()  # Assuming all_objects is available in FileStorage
+            return [city for city in all_cities if city.__class__ == City and city.state_id == self.id]
