8af117bc45e76e39c6ada8bc7a7e124ce2f607bc
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-21 04:59:30 +0200

    City class tweak

--- models/city.py
+++ models/city.py
@@ -4,16 +4,9 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import ForeignKey, String, Column
 from sqlalchemy.orm import relationship
 import os 
-
-
-
 class City(BaseModel, Base):
     __tablename__ = "cities"
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities",
-                              cascade="all, delete, delete-orphan")
-    else:
-        state_id = ""
-        name = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities",
+                            cascade="all, delete, delete-orphan")
