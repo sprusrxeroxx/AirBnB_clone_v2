cae7fcb5b77ec409a814ad95267f3ed86abb741f
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-21 05:16:16 +0200

    test file + city class update

--- models/city.py
+++ models/city.py
@@ -5,10 +5,13 @@ from sqlalchemy import ForeignKey, String, Column
 from sqlalchemy.orm import relationship
 import os 
 class City(BaseModel, Base):
+    """A city class, defines all cities in db"""
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities",
-                        cascade="all, delete, delete-orphan")
-    state_id = ""
-    name = ""
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete, delete-orphan")
+    else:
+        state_id = ""
+        name = ""
